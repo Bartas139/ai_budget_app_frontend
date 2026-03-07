2657b44b17a1c8f8c1d9ecd50ea35cd486eae040
Matyáš Charvát <bartas139@gmail.com>
2026-03-07 17:32:25 +0100

    vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   ssr: false,
-
+  nitro: {
+    preset: "vercel-static", // klíčové pro SPA na Vercelu
+  },
   modules: ["@nuxtjs/tailwindcss"],
 
   runtimeConfig: {
@@ -16,7 +18,11 @@ export default defineNuxtConfig({
       title: "Atlantis Budget",
       link: [
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
-        { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
         {
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500;600&family=DM+Serif+Display:ital@0;1&display=swap",
