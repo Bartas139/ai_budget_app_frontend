a73ea67a7123f5b970fe37d07c6973a544d1e129
Matyáš Charvát <bartas139@gmail.com>
2026-03-07 17:37:37 +0100

    vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,9 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:3000",
+      apiBase:
+        process.env.NUXT_PUBLIC_API_BASE ||
+        "https://ai-budget-app-theta.vercel.app",
     },
   },
 
